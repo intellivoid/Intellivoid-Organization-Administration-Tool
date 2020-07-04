a6881caa2639406e86cc31af295ec3b2469e801b
netkas <netkas@intellivoid.info>
2020-07-04 13:51:40 -0400

    Added project files

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Intellivoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Intellivoid Organization Administration Tool")]
+[assembly: AssemblyDescription("Administrates the Intellivoid Organization")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Intellivoid Technologies")]
+[assembly: AssemblyProduct("Intellivoid Organization Administration Tool")]
+[assembly: AssemblyCopyright("Copyright © Intellivoid Technologies 2017-2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d2d29db-b011-47c4-8393-afedce835a56")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
